a8316142d973515f0600bb9ef974e81268a9c2c4
akochetov <kochetov_anatoly@mail.ru>
2016-06-25 13:40:08 +0300

    added speakers settings, modified speakers.py to support pattern decode to string

--- Device/speakers.py
+++ Device/speakers.py
@@ -7,5 +7,11 @@ from device_signal import Signal
 print(sys.argv[1])
 signal = Signal(**json.loads(sys.argv[1]))
 irtx = IrTxDevice()
-irtx.SendOnce(signal.Pattern, "/home/pi/lircd.conf")
+pattern = list(signal.Pattern)
+
+key = ""
+for char_code in pattern:
+    key = key + chr(char_code)
+print(key)
+irtx.SendOnce(key, "/home/pi/lircd.conf")
 
