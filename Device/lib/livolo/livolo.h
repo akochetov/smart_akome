1d43ad9144aa085a00da760e046d61dc27d40ffb
akochetov <kochetov_anatoly@mail.ru>
2016-06-29 15:08:55 +0300

    Added button code constants

--- Device/lib/livolo/livolo.h
+++ Device/lib/livolo/livolo.h
@@ -11,6 +11,23 @@
 
 #include <wiringPi.h>
 
+//button code constants
+#define BUTTON_A	8
+#define BUTTON_B	16
+#define BUTTON_C	56
+#define BUTTON_D	42
+#define BUTTON_1	0
+#define BUTTON_2	96
+#define BUTTON_3	120
+#define BUTTON_4	24
+#define BUTTON_5	80
+#define BUTTON_6	48
+#define BUTTON_7	108
+#define BUTTON_8	12
+#define BUTTON_9	72
+#define BUTTON_10	40
+#define BUTTON_OFF	106
+
 class Livolo
 {
   public:
--- Device/lib/livolo/python_wrap.c
+++ Device/lib/livolo/python_wrap.c
@@ -1,6 +1,26 @@
 #include <python.h>
 #include "livolo.h"
 
+
+void define_constants(PyObject *module)
+{
+	btn_a = Py_BuildValue("b",  BUTTON_A);PyModule_AddObject(module, "BUTTON_A", btn_a);
+	btn_b = Py_BuildValue("b",  BUTTON_B);PyModule_AddObject(module, "BUTTON_B", btn_b);
+	btn_c = Py_BuildValue("b",  BUTTON_C);PyModule_AddObject(module, "BUTTON_C", btn_c);
+	btn_d = Py_BuildValue("b", BUTTON_D);PyModule_AddObject(module, "BUTTON_D", btn_d);
+	btn_1 = Py_BuildValue("b", BUTTON_1);PyModule_AddObject(module, "BUTTON_1", btn_1);
+	btn_2 = Py_BuildValue("b", BUTTON_2);PyModule_AddObject(module, "BUTTON_2", btn_2);
+	btn_3 = Py_BuildValue("b", BUTTON_3);PyModule_AddObject(module, "BUTTON_3", btn_3);
+	btn_4 = Py_BuildValue("b", BUTTON_4);PyModule_AddObject(module, "BUTTON_4", btn_4);
+	btn_5 = Py_BuildValue("b", BUTTON_5);PyModule_AddObject(module, "BUTTON_5", btn_5);
+	btn_6 = Py_BuildValue("b", BUTTON_6);PyModule_AddObject(module, "BUTTON_6", btn_6);
+	btn_7 = Py_BuildValue("b", BUTTON_7);PyModule_AddObject(module, "BUTTON_7", btn_7);
+	btn_8 = Py_BuildValue("b", BUTTON_8);PyModule_AddObject(module, "BUTTON_8", btn_8);
+	btn_9 = Py_BuildValue("b", BUTTON_9);PyModule_AddObject(module, "BUTTON_9", btn_9);
+	btn_10 = Py_BuildValue("b", BUTTON_10);PyModule_AddObject(module, "BUTTON_10", btn_10);
+	btn_OFF = Py_BuildValue("b", BUTTON_OFF);PyModule_AddObject(module, "BUTTON_OFF", btn_OFF);
+}
+
 // python function value = sendButton(pin, remoteID, code)
 static PyObject *sendButton(PyObject *self, PyObject *args)
 {
@@ -32,12 +52,11 @@ static PyObject *sendButton(PyObject *self, PyObject *args)
 	return 1;
 }
 
-
 static const char moduledocstring[] = "Controlling Livolo light switches with 433 Mhz";
 
 PyMethodDef livolo_methods[] = {
-	{"sendButton", py_sendButton, METH_VARARGS, "Send button code to Livolo switch\npin - wiringPi pi number (may differ from GPIO pin number)\nremoteID - ID of a physical or virtual remote, usually X*3 value where X is any integer, but other values may also work. Working codes: 6400, 26088, 23783.\ncode - button code. Key #A: 8, #B: 16, #C: 56, #D: 42. For numeric buttons: #1: 0, #2: 96, #3: 120, #4: 24, #5: 80, #6: 48, #7: 108, #8: 12, #9: 72; #10: 40, #OFF: 106"},
-   {NULL, NULL, 0, NULL}
+	{"sendButton", py_sendButton, METH_VARARGS, "Send button code to Livolo switch\npin - wiringPi pi number (may differ from GPIO pin number)\nremoteID - ID of a physical or virtual remote, usually X*3 value where X is any integer, but other values may also work. Working codes: 6400, 26088, 23783.\ncode - button code (use BUTTON_* constants or put alternative values)."},
+	{NULL, NULL, 0, NULL}
 };
 
 #if PY_MAJOR_VERSION > 2
