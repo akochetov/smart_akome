1d94046f9fe2a63bd8c56c3d8b3ec498cfbe4e40
akochetov <kochetov_anatoly@mail.ru>
2017-11-22 10:05:34 +0200

    Fixed ssh configuration settings

--- Install/tools.sh
+++ Install/tools.sh
@@ -1,10 +1,21 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
+
+echo "Configuring remote desktop..."
+
 sudo apt-get install ntpdate 
 sudo apt-get install tightvncserver
 sudo apt-get install xrdp
 sudo apt-get install maven
+
+echo "Configuring ssh..."
+
+sudo sed -i -e 's/#Port 22/Port 2722/g' /etc/ssh/sshd_config
+sudo sed -i -e 's/Port 22/Port 2722/g' /etc/ssh/sshd_config
+sudo /etc/init.d/ssh restart
+
+echo "Configuring java..."
 
 sudo update-java-alternatives --set jdk-8-oracle-arm32-vfp-hflt
 
